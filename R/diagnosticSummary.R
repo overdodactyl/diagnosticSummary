449a69fd149c1f5423db5e42b018374027dfc64e
overdodactyl <patjohns1216@gmail.com>
2024-01-06 14:55:16 -0700

    docs: remove old globalVariables

--- R/diagnosticSummary.R
+++ R/diagnosticSummary.R
@@ -6,16 +6,5 @@
 #' @name diagnosticSummary
 NULL
 
-## quiets concerns of R CMD check re: the .'s that appear ggplot2
-utils::globalVariables(c(
-  ".data"
-  # "lead_specificity",
-  # "predicted",
-  # "count",
-  # "label",
-  # "specificity",
-  # "sensitivity",
-  # "variable",
-  # "y",
-  # "threshold"
-))
+## quiets concerns of R CMD check for variables used in ggplot2
+utils::globalVariables(c(".data"))
